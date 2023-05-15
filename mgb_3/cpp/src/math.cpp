4be0cd2512b5278d04308b01abdfa409112086f7
Adelse <133372140+Adelse@users.noreply.github.com>
2023-05-15 21:54:26 +0300

    Add files via upload

---
+++ mgb_3/cpp/src/math.cpp
@@ -0,0 +1,10 @@
+#ifndef CPP_MATH_CPP
+#define CPP_MATH_CPP
+
+#include "math.h"
+
+int sum(int a, int b){
+    return a + b;
+}
+
+#endif //CPP_MATH_CPP
---
+++ mgb_3/cpp/src/math.h
@@ -0,0 +1,8 @@
+#ifndef CPP_MATH_H
+#define CPP_MATH_H
+
+
+int sum(int a, int b);
+
+
+#endif //CPP_MATH_H
